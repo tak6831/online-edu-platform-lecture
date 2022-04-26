a05f17c960887355fbc942ccab4c0a298992af68
Jeayoeb Kim <jykim@atones.co.kr>
2022-04-27 00:08:34 +0900

    feat: 결과 나오게 수정...

--- src/main/resources/rating.sql
+++ src/main/resources/rating.sql
@@ -1,16 +1,14 @@
 DROP TABLE IF EXISTS RATING;
-
 CREATE TABLE IF NOT EXISTS RATING
 (
-    rating_id   int      NOT NULL AUTO_INCREMENT COMMENT '별점 id',
-    lecture_id  int      NOT NULL COMMENT '강의 id',
-    user_id     int      NOT NULL COMMENT '작성자(학생) id',
-    rating      float    DEFAULT '0' COMMENT '별점',
-    comment     varchar(200)      NOT NULL COMMENT '코멘트',
-    created_at  datetime  DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
-    updated_at  datetime  DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일',
-    PRIMARY KEY (rating_id)
-);
+    rating_id   int      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    lecture_id  int      NOT NULL,
+    user_id     int      NOT NULL,
+    rating      float    DEFAULT '0',
+    comment     varchar(200)      NOT NULL,
+    created_at  TIMESTAMP  DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP  DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+    );
 
 INSERT INTO rating (rating_id, lecture_id, user_id, rating, comment, created_at, updated_at)
 VALUES (1, 1, 1, 5, 'lecture1-1', now(), now());
